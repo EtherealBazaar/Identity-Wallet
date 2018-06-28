153226d92dcd94e840a0f4900f18e6248334cbf6
Maxim Kovalov <max.kovalov@gmail.com>
2018-06-28 17:59:05 +0300

    refactor(models): refactor models to use sqkUtils WIP

--- src/main/controllers/models/app-setting.js
+++ src/main/controllers/models/app-setting.js
@@ -1,7 +1,7 @@
 // TODO: Implement settings model that doesn't suck
 
-module.exports = function(app, sqlLiteService) {
-	const TABLE_NAME = 'app_settings';
+const TABLE_NAME = 'app_settings';
 
-	return {};
+module.exports = {
+	TABLE_NAME
 };
